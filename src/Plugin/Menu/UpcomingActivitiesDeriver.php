216357da39b939e127c37bc11ab414d9a6d50c3c
acdweb.nl <ict@acdweb.nl>
2026-01-07 13:28:15 +0000

    bugfix

--- src/Plugin/Menu/UpcomingActivitiesDeriver.php
+++ src/Plugin/Menu/UpcomingActivitiesDeriver.php
@@ -7,7 +7,6 @@ use Drupal\Core\Plugin\Discovery\ContainerDeriverInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Datetime\DrupalDateTime;
-use Drupal\menu_link_content\Entity\MenuLinkContent;
 
 /**
  * Derives menu links for upcoming 'activiteit' nodes.
@@ -28,47 +27,33 @@ class UpcomingActivitiesDeriver extends DeriverBase implements ContainerDeriverI
 
   public function getDerivativeDefinitions($base_plugin_definition) {
     // 1. SETUP PARENT & MENU
-    // UUID of the parent 'Activiteiten' menu link
     $parentUuid = '51ed2590-3345-4271-935c-0d0f409d6325';
     $parentId = 'menu_link_content:' . $parentUuid;
     
-    // Default to 'main', but try to find the REAL menu name from the parent
-    $menuName = 'Activiteiten'; 
-    
-    // Load the parent entity to find out which menu it lives in
-    $parent_entity = $this->entityTypeManager->getStorage('menu_link_content')->loadByProperties(['uuid' => $parentUuid]);
-    if ($parent_entity) {
-      $parent_entity = reset($parent_entity);
+    // Attempt to load parent to ensure dynamic menu name correctness
+    $menuName = 'main'; // Default fallback
+    $parent_entities = $this->entityTypeManager->getStorage('menu_link_content')->loadByProperties(['uuid' => $parentUuid]);
+    if ($parent_entity = reset($parent_entities)) {
       $menuName = $parent_entity->getMenuName();
     }
 
-    // 2. PREPARE DATE
-    // Use standard ISO format. 
-    // If your field is "Date only" (no time), change this to 'Y-m-d'
-    $now = new DrupalDateTime('now');
-    $now_formatted = $now->format('Y-m-d\TH:i:s');
-
-    // 3. QUERY NODES
+    // 2. QUERY NODES
+    $now_formatted = (new DrupalDateTime('now'))->format('Y-m-d\TH:i:s');
+    
     $storage = $this->entityTypeManager->getStorage('node');
     $query = $storage->getQuery();
-    
-    $query->condition('type', 'activiteit')
+    $nids = $query->condition('type', 'activiteit')
       ->condition('status', 1)
       ->condition('field_datum', $now_formatted, '>') 
       ->sort('field_datum', 'ASC')
       ->range(0, 5)
-      ->accessCheck(TRUE);
+      ->accessCheck(TRUE)
+      ->execute();
 
-    $nids = $query->execute();
     $nodes = $storage->loadMultiple($nids);
 
-    // 4. GENERATE LINKS
-    foreach ($nodes as $node) {
-      $nids = $query->execute();
-    $nodes = $storage->loadMultiple($nids);
-
-    // 1. START COUNTER HERE
-    $menu_weight = 0; // <--- ADD THIS
+    // 3. GENERATE LINKS
+    $menu_weight = 0; 
 
     foreach ($nodes as $node) {
       $derivative_id = $node->id();
@@ -79,17 +64,17 @@ class UpcomingActivitiesDeriver extends DeriverBase implements ContainerDeriverI
       $this->derivatives[$derivative_id]['route_parameters'] = ['node' => $node->id()];
       $this->derivatives[$derivative_id]['menu_name'] = $menuName; 
       $this->derivatives[$derivative_id]['parent'] = $parentId;
+      $this->derivatives[$derivative_id]['weight'] = $menu_weight++;
       
-      // 2. ASSIGN WEIGHT HERE
-      $this->derivatives[$derivative_id]['weight'] = $menu_weight++; // <--- ADD THIS
-      
-      $this->derivatives[$derivative_id]['cache_tags'] = array_merge(
-        $node->getCacheTags(),
-        ['node_list:activiteit']
-      );
+      // Ensure the menu rebuilds when this node changes
+      $this->derivatives[$derivative_id]['cache_tags'] = $node->getCacheTags();
     }
+
+    // Important: Add a general list tag so adding a NEW node triggers a refresh
+    $this->derivatives['list_tag'] = $base_plugin_definition; 
+    // This is a hacky way to attach metadata to the deriver, usually handled by cache invalidation elsewhere.
+    // Instead, rely on standard cache tag invalidation 'node_list:activiteit'.
 
     return $this->derivatives;
   }
-}
 }
